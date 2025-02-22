396cb3db068316621df7e9dab9cf1b0f2439a677
Washington-Kimani <kimaniwashington39@gmail.com>
2025-02-22 09:33:01 +0300

    Make ready for hosting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 // import and connect to database
 import connectDb from "./server/configs/connectDb.js";
@@ -15,10 +16,16 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 
+// Get the current module's file path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name of the current module
+const __dirname = dirname(__filename);
+
 
 //basic middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, 'client', 'dist')));
 app.use(cors());
 
 // import and use routes
@@ -27,10 +34,10 @@ app.use('/api/items', routes);
 
 
 //general route
-app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-})
-
+app.get("*", (req, res) => {
+    const targetPath = join(__dirname, 'client', 'dist', 'index.html');
+    res.sendFile(targetPath);
+});
 
 //run server
 server.listen(PORT, () => {
